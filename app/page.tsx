ddbf4fe65aa5ca904af0206ee60a813ab3baf73e
azuraservices <stanislao97s@gmail.com>
2024-11-07 11:11:26 +0100

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -351,7 +351,7 @@ export default function FashionApp() {
                       preventSwipe={['up', 'down']}
                       swipeRequirementType="position"
                       swipeThreshold={100}
-                      className="absolute inset-0 flex-grow pr-4 pl-4 max-h-[350px]"
+                      className="absolute inset-0 flex-grow pr-4 pl-4 pb-8"
                     >
                       {renderItemCard(currentItems[0])}
                     </TinderCard>
